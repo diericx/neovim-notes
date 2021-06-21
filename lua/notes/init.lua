2e4a8a99f6555529ef3c0c117026a9e5f129c22f
Zac Holland <zachary@pubnub.com>
2021-06-21 12:40:00 -0700

    added custom bordered window that opens daily notes

---
+++ lua/notes/init.lua
@@ -0,0 +1,88 @@
+local api = vim.api
+local buf, win
+
+function replace_vars(str, vars)
+  -- Allow replace_vars{str, vars} syntax as well as replace_vars(str, {vars})
+  if not vars then
+    vars = str
+    str = vars[1]
+  end
+  return (string.gsub(
+    str,
+    "({([^}]+)})",
+    function(whole, i)
+      return vars[i] or whole
+    end
+  ))
+end
+
+local function open_window()
+  buf = api.nvim_create_buf(false, true)
+  local border_buf = api.nvim_create_buf(false, true)
+
+  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+  api.nvim_buf_set_option(buf, "filetype", "whid")
+
+  local width = api.nvim_get_option("columns")
+  local height = api.nvim_get_option("lines")
+
+  local win_height = math.ceil(height * 0.8 - 4)
+  local win_width = math.ceil(width * 0.8)
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
+
+  local border_opts = {
+    style = "minimal",
+    relative = "editor",
+    width = win_width + 2,
+    height = win_height + 2,
+    row = row - 1,
+    col = col - 1
+  }
+
+  local opts = {
+    style = "minimal",
+    relative = "editor",
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col
+  }
+
+  local border_lines = {"╔" .. string.rep("═", win_width) .. "╗"}
+  local middle_line = "║" .. string.rep(" ", win_width) .. "║"
+  for i = 1, win_height do
+    table.insert(border_lines, middle_line)
+  end
+  table.insert(border_lines, "╚" .. string.rep("═", win_width) .. "╝")
+  api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
+
+  local border_win = api.nvim_open_win(border_buf, true, border_opts)
+  win = api.nvim_open_win(buf, true, opts)
+  vim.cmd(
+    replace_vars {
+      [[
+    augroup Notes
+      autocmd!
+      au WinClosed {win_id} silent bwipeout! {buf} 
+    augroup END
+  ]],
+      buf = border_buf,
+      win_id = win
+    }
+  )
+
+  api.nvim_win_set_option(win, "cursorline", true) -- it highlight line with the cursor on it
+
+  -- we can add title already here, because first line will never change
+  api.nvim_buf_add_highlight(buf, -1, "WhidHeader", 0, 0, -1)
+
+  vim.api.nvim_command("edit ~/Nextcloud/Obsidian/OBSNotes/Daily/" .. os.date("%Y-%m-%d") .. ".md")
+  vim.api.nvim_command("lcd ~/Nextcloud/Obsidian/OBSNotes")
+end
+
+return {
+  printWindowSize = printWindowSize,
+  notesPopup = notesPopup,
+  open_window = open_window
+}
--- plugin/your-first-plugin.vim
+++
@@ -1,9 +0,0 @@
-fun! YourFirstPlugin()
-  " don't forget to remove this...
-  lua for k in pairs(package.loaded) do if k:match("^your%-first%-plugin") then package.loaded[k] = nil end end
-  lua require("your-first-plugin").printWindowSize()
-endfun
-
-augroup YourFirstPlugin
-  autocmd!
-augroup END
